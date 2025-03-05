63703d80098fe6fff530623783e6533f9559fa26
John Manhart <manhartjohn@gmail.com>
2025-03-04 20:29:36 -0800

    fixing something dumb that I missed 🎵 Taylor Swift - Shake It Off

--- cli.js
+++ cli.js
@@ -2,6 +2,24 @@
 
 const { execSync } = require("child_process");
 
+// Sample List of Fake Songs
+const fakeSongs = [
+  "🎵 Daft Punk - Harder, Better, Faster, Stronger",
+  "🎵 The Beatles - Hey Jude",
+  "🎵 Radiohead - Creep",
+  "🎵 Queen - Bohemian Rhapsody",
+  "🎵 Taylor Swift - Shake It Off",
+  "🎵 Kendrick Lamar - HUMBLE.",
+  "🎵 Nirvana - Smells Like Teen Spirit",
+  "🎵 OutKast - Hey Ya!",
+  "🎵 Billie Eilish - Bad Guy",
+  "🎵 The Rolling Stones - Paint It Black",
+];
+
+// Function to pick a random song
+const getRandomSong = () =>
+  fakeSongs[Math.floor(Math.random() * fakeSongs.length)];
+
 const args = process.argv.slice(2);
 if (!args.length) {
   console.error("Usage: music-commit 'Your commit message {{ADD MUSIC}}'");
@@ -9,17 +27,17 @@ if (!args.length) {
 }
 
 // Extract commit message from arguments
-let commitMessage = args.join(" "); // Changed from `const` to `let`
+let commitMessage = args.join(" ");
 
-// Fake music data for now (this will be replaced with real music fetching)
-const fakeSong = "🎵 The Rolling Stones - Jumpin Jack Flash";
+// Get Random Song
+const randomSong = getRandomSong();
 
 // Check if the commit message contains {{ADD MUSIC}}
 if (commitMessage.includes("{{ADD MUSIC}}")) {
-  commitMessage = commitMessage.replace("{{ADD MUSIC}}", fakeSong);
+  commitMessage = commitMessage.replace("{{ADD MUSIC}}", randomSong);
 } else {
   console.log("⚠️ No {{ADD MUSIC}} found. Adding music automatically...");
-  commitMessage += ` ${fakeSong}`;
+  commitMessage += ` ${randomSong}`;
 }
 
 // Run git commit
